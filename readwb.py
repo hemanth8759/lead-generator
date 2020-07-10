0fbe3365ad4e1cfb5d7c6de553a0b201196946c0
Kondapalli Hemanth <hemanth.k@exeterpremedia.com>
2020-07-10 10:53:13 +0530

    start

--- readwb.py
+++ readwb.py
@@ -1,6 +1,9 @@
 import requests as req
 
 def get_webpage(url):
-    resp = req.get("http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm")
+    resp = req.get(url)
 
-    print(resp.text)
+    return resp.text
+
+
+get_webpage("http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm")

23288a5d7222c03c4c970b0b95efd558b821a362
Kondapalli Hemanth <hemanth.k@exeterpremedia.com>
2020-07-10 11:50:11 +0530

    first commit

--- readwb.py
+++ readwb.py
@@ -2,8 +2,13 @@ import requests as req
 
 def get_webpage(url):
     resp = req.get(url)
+    if(resp.status_code != 200):
+        return None
+    else:
+        get_webpage_text(resp.text)
+        return resp.text
 
-    return resp.text
-
+def get_webpage_text(html):
+    return str(html)
 
 get_webpage("http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm")

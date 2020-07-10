f9477b1a3c9b46dcea64ed9a8c8619a7f28f6b48
Kondapalli Hemanth <hemanth.k@exeterpremedia.com>
2020-07-10 12:15:41 +0530

    2

--- readwb.py
+++ readwb.py
@@ -6,9 +6,19 @@ def get_webpage(url):
         return None
     else:
         get_webpage_text(resp.text)
+        get_list(resp.text)
         return resp.text
 
 def get_webpage_text(html):
     return str(html)
+
+def get_list(page_html):
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(page_html, 'html.parser')
+    companies = []
+    for link in soup.find_all('a', attrs = {'class':'100link'}):
+        companies.append(link.get_text()+" = "+link.get('href'))
+    print(companies)
+
 
 get_webpage("http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm")

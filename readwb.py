017081146b795768225c829b937d9f87a0fc2a57
Kondapalli Hemanth <hemanth.k@exeterpremedia.com>
2020-07-11 11:11:55 +0530

    update

--- readwb.py
+++ readwb.py
@@ -6,9 +6,6 @@ def get_webpage(url):
     if(resp.status_code != 200):
         return None
     else:
-        get_webpage_text(resp.text)
-        get_list(resp.text)
-        get_contact_page_link(resp.text)
         return resp.text
 
 def get_webpage_text(html):
@@ -30,25 +27,34 @@ def get_contact_page_link(html):
     for i in soup1.find_all('a', attrs={'class': '100link'}):
         if("http://www.econtentmag.com/Articles/ArticleReader" not in str(i.get('href'))):
             comp.append(i.get('href'))
-    for i in comp:
-            res = req.get(str(i))
+    for i in range(15,26):
+        try:
+            res = req.get(str(comp[i]))
             soup = BeautifulSoup(res.text, 'html.parser')
             for j in soup.find_all('a', href=True):
                 if ("Contact" in str(j)):
-                    if ("http:" not in str(j.get('href'))):
-                        addLinks.append(str(i) + str(j.get('href')))
+                    if ("http" not in str(j.get('href'))):
+                        addLinks.append(str(comp[i]) + str(j.get('href')))
                     else:
                         addLinks.append(str(j.get('href')))
                 elif ("About" in str(j)):
-                    if ("http:" not in str(j.get('href'))):
-                        addLinks.append(str(i) + str(j.get('href')))
+                    if ("http" not in str(j.get('href'))):
+                        addLinks.append(str(comp[i]) + str(j.get('href')))
                     else:
                         addLinks.append(str(j.get('href')))
-    addLinks = list(dict.fromkeys(addLinks))
-    return addLinks
+        except req.exceptions.HTTPError as e:
+            print(e)
+    add_links = list(dict.fromkeys(addLinks))
+    return add_links
 
 
-
-
-
-get_webpage("http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm")
+if __name__ == "__main__":
+    wbp = get_webpage(
+        "http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm")
+    # print(wbp)
+    # wbpt = get_webpage_text(wbp)
+    # print(wbpt)
+    # list = get_list(wbp)
+    # print(list)
+    contacts = get_contact_page_link(wbp)
+    print(contacts)
